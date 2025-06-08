6b2fb486e60d7754182236d7623d643da73e5fd5
Nathan Frankel <frankel.nathan@gmail.com>
2025-06-08 14:54:48 -0700

    add routes to add and delete contacts

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -14,3 +14,44 @@ export const addNewContact = async (req, res) => {
     res.status(500).json({ message: " Error saving contact" }); // Handle errors
   }
 };
+
+export const getContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find({});
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const getContactWithId = async (req, res) => {
+  try {
+    const contact = await Contact.findById(req.params.contactId);
+    res.json(contact);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const updateContact = async (req, res) => {
+  try {
+    const contact = await Contact.findOneAndUpdate(
+      { _id: req.params.contactId },
+      req.body,
+      { new: true },
+      (req, contact)
+    );
+    res.json(contact);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const deleteContact = async (req, res) => {
+  try {
+    await Contact.deleteOne({ _id: req.params.contactId });
+    res.json({ message: "Successfully deleted contact" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,27 +1,29 @@
-import { addNewContact } from "../controllers/crmController";
+import {
+  addNewContact,
+  deleteContact,
+  getContacts,
+  getContactWithId,
+  updateContact,
+} from "../controllers/crmController";
 
 const routes = (app) => {
   app
     .route("/contact")
-    .get(
-      (req, res, next) => {
-        // middleware
-        console.log(`Request from: ${req.originalUrl}`);
-        console.log(`Request type: ${req.method}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send("GET request successful!");
-      }
-    )
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContacts)
 
     .post(addNewContact);
 
   app
     .route("/contact/:contactId")
-    .put((req, res) => res.send("PUT request successful!"))
+    .get(getContactWithId)
+    .put(updateContact)
 
-    .delete((req, res) => res.send("DELETE request successful!"));
+    .delete(deleteContact);
 };
 
 export default routes;
